947f8319ec5c291fe62bce3f2587b52e2ec27f0f
mauro-ferrario <hello@mauroferrario.com>
2018-10-15 13:04:17 +0200

    first commit

---
+++ src/js/controllers/d3-class-name.js
@@ -0,0 +1,9 @@
+import * as d3 from "d3";
+require ("../../css/components/d3-class-name.scss");
+
+export default class{
+    constructor(el){
+        this.el = el;
+        console.log("D3 class name test");
+    }
+}
